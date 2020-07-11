b534735da8fc9e3b7406d107486ff284e044d276
nidhi <nidhi.gupta_cs18@gla.ac.in>
2020-07-11 13:47:22 +0530

    feilds defined

--- src/student/StudentConnection.java
+++ src/student/StudentConnection.java
@@ -1,4 +1,11 @@
 package student;
 
+import connection.Connection;
+
 public class StudentConnection {
+    private static final String database = "jdbc:mysql://localhost:3306/studentmanagementsyatem?useTimezone=true&serverTimezone=UTC";
+    private static final String username = "root";
+    private static final String password = "";
+    public Connection connection = null;
+
 }
