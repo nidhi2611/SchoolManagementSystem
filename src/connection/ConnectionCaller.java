8db9be2b9394fcf68e00d3114263b02ca9f4b570
nidhi <nidhi.gupta_cs18@gla.ac.in>
2020-07-11 15:35:49 +0530

    object created for student class calling

---
+++ src/connection/ConnectionCaller.java
@@ -0,0 +1,12 @@
+package connection;
+
+import student.StudentConnection;
+
+import java.sql.SQLException;
+
+public class ConnectionCaller {
+    public static void main(String rags[]) throws SQLException {
+        StudentConnection student = new StudentConnection();
+
+    }
+}
