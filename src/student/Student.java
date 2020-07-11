dad3172f1f0ef7d390e8cdcaffcf7c6313edcb15
nidhi <nidhi.gupta_cs18@gla.ac.in>
2020-07-11 17:31:08 +0530

    constructor defined

--- src/student/Student.java
+++ src/student/Student.java
@@ -4,7 +4,13 @@ public class Student {
     private String Name;
     private int University_Roll_No;
     private String Course;
-    private int Contact_String;
+    private int Contact_Number;
 
+    public Student(String s, int urno, String c, int phno) {
+        Name = s;
+        University_Roll_No = urno;
+        Course = c;
+        Contact_Number = phno;
+    }
 
 }
