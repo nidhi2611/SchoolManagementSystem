b8e9972bf84ac202a9b9d7bd216f7309e01de13d
nidhi <nidhi.gupta_cs18@gla.ac.in>
2020-07-11 18:09:53 +0530

    student creation function written

--- src/principal/StudentCreation.java
+++ src/principal/StudentCreation.java
@@ -1,5 +1,25 @@
 package principal;
 
+import student.Student;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class StudentCreation {
+    public boolean isCreated = false;
+    Scanner sc = new Scanner(System.in);
+
+    public void createNewStudent(Connection connection, Student student) throws SQLException {
+        String query = "INSERT INTO studentcorner VALUES(?,?,?,?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, student.getName());
+        preparedStatement.setInt(2, student.getUniversity_Roll_No());
+        preparedStatement.setString(3, student.getCourse());
+        preparedStatement.setInt(4, student.getContact_Number());
+        isCreated = preparedStatement.executeUpdate() >= 1;
+    }
+
 
 }
